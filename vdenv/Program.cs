3f8ab5ad9f7539b0227f86561aa60320770c04d4
Freesia <freesia@studiofreesia.com>
2025-02-09 03:33:48 +0900

    デスクトップの設定を表示するInfoメソッドを追加しました。設定ファイルが存在しない場合のエラーメッセージも実装しました。

--- vdenv/Program.cs
+++ vdenv/Program.cs
@@ -14,6 +14,7 @@ string configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFo
 var app = ConsoleApp.Create();
 app.Add("", Root);
 app.Add("init", Init);
+app.Add("info", Info);
 app.Run(args);
 
 /// <summary>
@@ -122,4 +123,39 @@ async Task Init(bool reset = false, bool prune = false)
     // 書き込み
     using var fs = new FileStream(configPath, FileMode.Create, FileAccess.Write);
     await fs.WriteAsync(YamlSerializer.Serialize(new RootConfig(newDesktops)));
+}
+
+/// <summary>
+/// 現在のデスクトップの設定を表示
+/// </summary>
+/// <param name="all">全ての設定を表示</param>
+async Task Info(bool all = false)
+{
+    if (!File.Exists(configPath))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Config file not found. Run `vdenv init`.");
+        Console.ResetColor();
+        return;
+    }
+    var buf = await File.ReadAllBytesAsync(configPath);
+    if (all)
+    {
+        Console.Write(Encoding.UTF8.GetString(buf));
+        return;
+    }
+
+    var config = YamlSerializer.Deserialize<RootConfig>(buf);
+
+    var current = VirtualDesktop.Current;
+    if (config.Desktops?.TryGetValue(current.Id, out var desktop) ?? false)
+    {
+        Console.WriteLine(YamlSerializer.SerializeToString(desktop));
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"`{current.Name}({current.Id})` not found in config.");
+        Console.ResetColor();
+    }
 }
