9d81e06cec3edf32e0eace9aa77527517f3d4586
Freesia <freesia@studiofreesia.com>
2025-02-09 01:30:18 +0900

    既存のデスクトップ設定を読み込み、新しいデスクトップ設定を作成する機能を追加しました。古いデスクトップの存在を管理し、設定を更新して書き込む処理を実装しました。

--- vdenv/Program.cs
+++ vdenv/Program.cs
@@ -16,7 +16,35 @@ app.Run(args);
 
 async Task Init()
 {
-    var config = new RootConfig(VirtualDesktop.GetDesktops().ToDictionary(d => d.Id, d => new DesktopConfig(true, [], string.Empty, string.Empty)));
+    IReadOnlyDictionary<Guid, DesktopConfig>? oldDesktops = null;
+    if (File.Exists(configPath))
+    {
+        var buf = await File.ReadAllBytesAsync(configPath);
+        oldDesktops = YamlSerializer.Deserialize<RootConfig>(buf).Desktops;
+    }
+
+    var newDesktops = new Dictionary<Guid, DesktopConfig>();
+
+    foreach (var desktop in VirtualDesktop.GetDesktops())
+    {
+        if (oldDesktops?.TryGetValue(desktop.Id, out var oldDesktop) ?? false)
+        {
+            newDesktops.Add(desktop.Id, oldDesktop);
+        }
+        else
+        {
+            newDesktops.Add(desktop.Id, new DesktopConfig(true, [], "", ""));
+        }
+    }
+    if (oldDesktops is not null)
+    {
+        foreach (var (id, desktop) in oldDesktops)
+        {
+            newDesktops.TryAdd(id, desktop with { Exists = false });
+        }
+    }
+
+    // 書き込み
     using var fs = new FileStream(configPath, FileMode.Create, FileAccess.Write);
-    await fs.WriteAsync(YamlSerializer.Serialize(config));
+    await fs.WriteAsync(YamlSerializer.Serialize(new RootConfig(newDesktops)));
 }
