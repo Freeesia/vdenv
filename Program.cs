e2ed2031946d8be6d1336145e1b5fd96a5c6d685
Freesia <freesia@studiofreesia.com>
2025-02-08 20:36:16 +0900

    Visual Studioの一時ファイルとビルド結果を無視するために.gitignoreを更新し、プログラムの構造を整理しました。新しいプロジェクトファイルとメインプログラムを追加しました。

--- Program.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-using ConsoleAppFramework;
-
-// ConsoleApp.Run(args, (int foo, int bar) => Console.WriteLine($"Sum: {foo + bar}"));
---
+++ vdenv/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+using ConsoleAppFramework;
+
+ConsoleApp.Run(args, (int foo, int bar) => Console.WriteLine($"Sum: {foo + bar}"));
